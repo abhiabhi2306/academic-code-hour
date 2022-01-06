c34448b55ae6a6cb80d1f854fd995e974d8fb149
Abhishek Sidharth <32158780+abhiabhi2306@users.noreply.github.com>
2022-01-06 13:19:42 +0530

    Create digits.py

---
+++ abhyas-1/orange/digits.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findDigits' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def findDigits(n):
+    nt = str(n)
+    nt = list(nt)
+    divs = 0
+    for i in nt:
+        if int(i)==0:
+            continue
+        if n%int(i)==0:
+            divs+=1
+    
+    return divs
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
